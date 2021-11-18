3f2fdf0e83b23afad3226e84a38c4e02963a96d7
JanS241 <55439224+JanS241@users.noreply.github.com>
2021-11-18 12:01:58 +0100

    Add files via upload

---
+++ function_exercise1.js
@@ -0,0 +1,32 @@
+// function basiscs: exercise
+
+//  test   console.log('hoi');
+
+const pizzabasic = function() {
+  console.log('#1 Collect the ingeredients');
+  console.log('#2 Take a big skillet');
+  console.log('#3 Get your favorite oil off the shelf');
+  console.log('#4 Find a good recipe book');
+  console.log('#5 Good luck!');  
+}
+
+pizzabasic();
+pizzabasic();
+pizzabasic();
+
+const sushibasic = function(){
+    console.log('#1 find a good recipe book');
+    console.log('#2 go to store and buy ingredients');
+    console.log('#3 ask your neighbors to perpare it');
+    console.log('#4 enjoy your meal');
+}
+sushibasic();
+
+//
+sushibasic();
+sushibasic();
+sushibasic();
+pizzabasic();
+pizzabasic();
+
+
